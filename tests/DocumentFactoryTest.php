547129519cc1bb5ee0ea92a82d1d1a5d0b06c11f
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-04-19 17:16:42 +0200

    now using DocumentFactory to simplify Document instantiation

---
+++ tests/DocumentFactoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TheCodingMachine\Gotenberg;
+
+use GuzzleHttp\Psr7\LazyOpenStream;
+use PHPUnit\Framework\TestCase;
+
+class DocumentFactoryTest extends TestCase
+{
+    function testMake()
+    {
+        // case 1: uses a file path.
+        $document = DocumentFactory::makeFromPath('file.pdf', __DIR__ . '/assets/file.pdf');
+        $this->assertNotEmpty($document->getFileStream());
+
+        // case 2: uses a stream.
+        $document = DocumentFactory::makeFromStream('file.pdf', new LazyOpenStream(__DIR__ . '/assets/file.pdf', 'r'));
+        $this->assertNotEmpty($document->getFileStream());
+    }
+}
